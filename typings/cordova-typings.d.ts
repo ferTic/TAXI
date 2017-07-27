636b0467a4cad9415f4afb706f30bc6d82aaa241
ferTic <30512146+ferTic@users.noreply.github.com>
2017-07-27 18:58:00 -0500

    Add files via upload

---
+++ typings/cordova-typings.d.ts
@@ -0,0 +1,11 @@
+
+/// <reference path="..\.vscode\typings\cordova\cordova.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\FileSystem.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\Device.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\Media.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\Vibration.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\NetworkInformation.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\Camera.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\FileTransfer.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\Dialogs.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\DeviceOrientation.d.ts"/>
